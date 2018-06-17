c0399304f43456e281b831eccf18a083ed12cd9e
AshV <ashish6318@gmail.com>
2018-06-18 01:28:38 +0530

    written code for word count

--- WordCountWorkflow/WordCountWorkflow/WordCount.cs
+++ WordCountWorkflow/WordCountWorkflow/WordCount.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Xrm.Sdk.Workflow;
+using System.Activities;
 
 namespace WordCountWorkflow
 {
-    public class WordCount
+    public class WordCount : CodeActivity
     {
+        [RequiredArgument]
+        [Input("Input Text")]
+        public InArgument<string> InputText { get; set; }
+
+        [Output("Word Count")]
+        public OutArgument<int> CountOfWords { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            this.CountOfWords.Set(
+                context,
+                this.InputText.Get<string>(context).Split(' ').Length);
+        }
     }
 }
